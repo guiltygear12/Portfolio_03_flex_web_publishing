2c23968986c69fa86099cd140187e20e3e45ce88
mylove700757 <mylove700757@gmail.com>
2022-09-04 19:40:15 +0900

    flex를 기반으로하는 반응형 웹사이트 만들기

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
+// 헤더
+// 헤더 스크롤 이벤트
+$(window).scroll(function () {
+	if ($(window).scrollTop() > 50) {
+		$("header, .gototop").addClass("active");
+	} else {
+		$("header, .gototop").removeClass("active");
+	}
+});
+
 $(".myslider").slick({
 	dots: true,
 	infinite: true,
 	speed: 300,
 	slidesToShow: 3,
 	slidesToScroll: 3,
-    autoplay: false,
-    speed: 500,
+	autoplay: false,
+	speed: 500,
 	responsive: [
 		{
 			breakpoint: 1024,
 			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3,
+				slidesToShow: 1,
+				slidesToScroll: 1,
 				infinite: true,
 				dots: true,
 			},
@@ -19,8 +29,8 @@ $(".myslider").slick({
 		{
 			breakpoint: 600,
 			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2,
+				slidesToShow: 1,
+				slidesToScroll: 1,
 			},
 		},
 		{
@@ -34,4 +44,12 @@ $(".myslider").slick({
 		// settings: "unslick"
 		// instead of a settings object
 	],
+});
+// trigger
+$(".trigger").click(function () {
+	$(this).toggleClass("active");
+	$(".gnb").toggleClass("active");
+});
+$(".gnb li a").click(function () {
+	$(".gnb, .trigger").removeClass("active");
 });
